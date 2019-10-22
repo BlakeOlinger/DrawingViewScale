909cce78199a382d06953112a0c16a3707f4347d
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-22 10:33:04 -0400

    -- daemon 1.0 complete

--- DrawingViewScale/Program.cs
+++ DrawingViewScale/Program.cs
@@ -13,15 +13,32 @@ namespace DrawingViewScale
         {
             var swInstance = new SldWorks.SldWorks();
             var model = (ModelDoc2)swInstance.ActiveDoc;
+            var drawing = (DrawingDoc)swInstance.ActiveDoc;
 
             // read app data to get the path of the drawing config file to read
             var appDataPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
             var appDataLines = System.IO.File.ReadAllLines(appDataPath);
             var configPath = appDataLines[0];
             var configLines = System.IO.File.ReadAllLines(configPath);
-            
-            // read from config - assume "\"Drawing View <#> Scale\"= #:#"
-            // get value
+
+            // get requested drawing view scale value from config
+            var drawingView1ScaleString = configLines[0].Split('=')[1].Trim();
+            var drawingView1ScaleArray = new double[2];
+            drawingView1ScaleArray[0] = Double.Parse(drawingView1ScaleString.ToCharArray()[0] + "");
+            drawingView1ScaleArray[1] = Double.Parse(drawingView1ScaleString.ToCharArray()[2] + "");
+            var drawingView1Scale = drawingView1ScaleArray[0] / drawingView1ScaleArray[1];
+
+            // get drawing active view sheet
+            var activeView = (View)drawing.GetFirstView();
+
+            // get first drawing view
+            activeView = (View)activeView.GetNextView();
+
+            // set View::ScaleRatio value
+            activeView.ScaleDecimal = drawingView1Scale;
+
+            // rebuild to reflect changes
+            model.EditRebuild3();
         }
 
         private static void displayLines(string[] lines)
@@ -29,6 +46,25 @@ namespace DrawingViewScale
             foreach (string line in lines)
             {
                 Console.WriteLine(line);
+            }
+        }
+        private static void displayLines(string line)
+        {
+            Console.WriteLine(line);
+        }
+        private static void displayLines(int line)
+        {
+            Console.WriteLine(line);
+        }
+        private static void displayLines(double line)
+        {
+            Console.WriteLine(line);
+        }
+        private static void displayLines(double[] lines)
+        {
+            foreach (double number in lines)
+            {
+                Console.WriteLine(number);
             }
         }
     }
