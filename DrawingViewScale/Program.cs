c752e2de69d8ccec6f508242762e588601390cbe
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-22 09:52:30 -0400

    Add project files.

---
+++ DrawingViewScale/Program.cs
@@ -0,0 +1,35 @@
+﻿using SldWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingViewScale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swInstance = new SldWorks.SldWorks();
+            var model = (ModelDoc2)swInstance.ActiveDoc;
+
+            // read app data to get the path of the drawing config file to read
+            var appDataPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\app data\rebuild.txt";
+            var appDataLines = System.IO.File.ReadAllLines(appDataPath);
+            var configPath = appDataLines[0];
+            var configLines = System.IO.File.ReadAllLines(configPath);
+            
+            // read from config - assume "\"Drawing View <#> Scale\"= #:#"
+            // get value
+        }
+
+        private static void displayLines(string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
